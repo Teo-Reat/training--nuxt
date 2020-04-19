8656e2a31ac8cb2a2a3d1887f87935fedb2e0ea8
Teo Reat <teo.reat@gmail.com>
2020-04-19 11:18:10 +0300

    First commit

---
+++ components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <ul class="flex">
+      <li class="mr-6">
+        <nuxt-link class="text-blue-500 hover:text-blue-800" to="/" exact active-class="font-bold" no-prefetch>
+          Home
+        </nuxt-link>
+      </li>
+      <li class="mr-6">
+        <nuxt-link class="text-blue-500 hover:text-blue-800" to="/about" active-class="font-bold">
+          About
+        </nuxt-link>
+      </li>
+      <li>
+        <nuxt-link class="text-blue-500 hover:text-blue-800" to="/users" active-class="font-bold">
+          Users
+        </nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/users/_id.vue
@@ -0,0 +1,13 @@
+<template>
+    <section>
+      <h1>User {{ $route.params.id }}</h1>
+    </section>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
